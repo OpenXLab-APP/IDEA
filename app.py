e375e6b45e7a51bdc8b906966313e0338b886b9f
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-11-08 17:47:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import argparse
 import os
 os.system("pip install ftfy regex tqdm")
 os.system("pip install git+https://github.com/openai/CLIP.git")
-os.system('cd models/UniPose/ops')
-os.system('python setup.py build install')
-os.system('cd ../..')
+os.system('os.system('cd models/UniPose/ops && python setup.py build install')
 
 import sys
 import io
