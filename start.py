f4eeba1cc6430c2b07a8710f77aadf8f5d45b868
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-11-08 19:38:49 +0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+build_install_command = "cd models/UniPose/ops && python setup.py build install"
+subprocess.run(build_install_command, shell=True, check=True)
+
+home_directory = "/home/xlab-app-center/"
+os.chdir(home_directory)
+
+app_command = "python app.py"
+subprocess.run(app_command, shell=True, check=True)
