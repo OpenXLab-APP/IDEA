03777c9ffb2e452c39cd51c7dd03795ea7b08c5b
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-11-09 12:47:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,7 @@ def text_encoding(instance_names, keypoints_names, model, device):
         kpt_text_embeddings.append(text_features)
 
     kpt_text_embeddings = torch.cat(kpt_text_embeddings, dim=0)
-
-
     return ins_text_embeddings, kpt_text_embeddings
-
-
-
 
 
 
@@ -354,19 +349,12 @@ checkpoint_path = "./unipose_swint.pth"  # change the path of the model
 model = load_model(config_file, checkpoint_path, cpu_only=False)
 
 if __name__ == "__main__":
-MARKDOWN = \
-"""
-## UniPose: Detecting Any Keypoints
 
-[GitHub](https://github.com/IDEA-Research/UniPose) | [Paper](http://arxiv.org/abs/2310.08530) | [Project Page](https://yangjie-cv.github.io/UniPose/)
-
-If UniPose is helpful for you, please help star the GitHub Repo. Thanks!
-"""
     block = gr.Blocks().queue()
     with block:
         with gr.Row():
             with gr.Column():
-                input_image = gr.Image(source='upload', type="pil")
+                input_image = gr.Image(type="pil")
                 instance_prompt = gr.Textbox(label="Instance Prompt")
                 keypoint_example = gr.Textbox(label="Keypoint Example",info="Support predefined keypoints: 1) Articulated Objects: person, face, hand, animal_in_AnimalKindom, animal_in_AP10K, animal_face, fly, locust; 2) Rigid Objects: car, table, chair, bed, sofa, swivelchair; 3) Soft Objects: short_sleeved_shirt, long_sleeved_outwear, short_sleeved_outwear, sling, vest, long_sleeved_dress, long_sleeved_shirt, trousers, sling_dress, vest_dress, skirt, short_sleeved_dress, shorts")
                 run_button = gr.Button(label="Run")
