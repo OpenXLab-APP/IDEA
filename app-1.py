c5683a63b0ce919a854bb8dc1fa4ac1ff01f68be
OpenXLab-APP <141816413+OpenXLab-APP@users.noreply.github.com>
2023-11-13 17:04:05 +0800

    Update app-1.py

--- app-1.py
+++ app-1.py
@@ -354,17 +354,19 @@ checkpoint_path = "./unipose_swint.pth"  # change the path of the model
 model = load_model(config_file, checkpoint_path, cpu_only=False)
 
 if __name__ == "__main__":
-MARKDOWN = \
-"""
-## UniPose: Detecting Any Keypoints
-
-[GitHub](https://github.com/IDEA-Research/UniPose) | [Paper](http://arxiv.org/abs/2310.08530) | [Project Page](https://yangjie-cv.github.io/UniPose/)
-
-If UniPose is helpful for you, please help star the GitHub Repo. Thanks!
-"""
+    MARKDOWN = \
+    """
+    ## UniPose: Detecting Any Keypoints
+    
+    [GitHub](https://github.com/IDEA-Research/UniPose) | [Paper](http://arxiv.org/abs/2310.08530) | [Project Page](https://yangjie-cv.github.io/UniPose/)
+    
+    If UniPose is helpful for you, please help star the GitHub Repo. Thanks!
+    """
+    
     block = gr.Blocks().queue()
     with block:
         with gr.Row():
+            gr.Markdown(MARKDOWN)
             with gr.Column():
                 input_image = gr.Image(source='upload', type="pil")
                 instance_prompt = gr.Textbox(label="Instance Prompt")
